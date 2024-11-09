b4e2a3a2ad3e1428bbdddc6d1e010b0da90295b3
melido <enrriquebello10@gmail.com>
2024-11-09 14:43:20 -0500

    adding 1 line connect db

--- server/index.ts
+++ server/index.ts
@@ -1,13 +1,16 @@
-import express, {Express, Request, Response} from 'express';
 import cors from "cors";
 import dotenv from "dotenv";
-import {connectDB} from "./config/database";
+import express, { Express } from 'express';
+import { connectDB } from "./config/database";
 import userRoutes from './routes/userRoutes';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 5001;
+
+// Connect to the database
+connectDB();
 
 // Middleware
 app.use(express.json());
