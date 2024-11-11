79f340c66d7232993cafc8dbbbfe95a6934613bd
Andres Rodriguez <arodrig068@citymail.cuny.edu>
2024-11-10 19:14:34 -0500

    RentaRoomie Project

--- App.jsx
+++ App.jsx
@@ -1,5 +1,10 @@
 import * as React from 'react';
 import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './src/screens/HomeScreen';
+import LoginScreen from './src/screens/LoginScreen';
+import SignupScreen from './src/screens/SignupScreen';
 
 
 
@@ -7,7 +12,13 @@ const Stack = createNativeStackNavigator();
 
 function App() {
   return (
-
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name={"Home"} component={HomeScreen} />
+        <Stack.Screen name={"LOGIN"} component={LoginScreen}/>
+        <Stack.Screen name={"SIGNUP"} component={SignupScreen}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
