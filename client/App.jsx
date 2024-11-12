5a769a65454743c8f3b0b66438609970bf1de998
Andres Rodriguez <arodrig068@citymail.cuny.edu>
2024-11-11 21:37:03 -0500

    Profile Creation Screen

--- client/App.jsx
+++ client/App.jsx
@@ -5,6 +5,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './src/screens/HomeScreen';
 import LoginScreen from './src/screens/LoginScreen';
 import SignupScreen from './src/screens/SignupScreen';
+import ProfileCreationScreen from './src/screens/ProfileCreationScreen';
 
 
 
@@ -17,6 +18,7 @@ function App() {
         <Stack.Screen name={"Home"} component={HomeScreen} />
         <Stack.Screen name={"LOGIN"} component={LoginScreen}/>
         <Stack.Screen name={"SIGNUP"} component={SignupScreen}/>
+        <Stack.Screen name={"PROFILECREATION"} component={ProfileCreationScreen}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
